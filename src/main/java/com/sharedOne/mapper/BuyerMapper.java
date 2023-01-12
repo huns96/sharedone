8feb14c3baf875425f2e9e2fe0518c92e7f3265b
ay <allzip1117@gmail.com>
2023-01-12 17:39:19 +0900

    머지 전 커밋3

--- src/main/java/com/sharedOne/mapper/BuyerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sharedOne.mapper;
-
-import com.sharedOne.domain.BuyerDto;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BuyerMapper {
-    BuyerDto selectBuyers();
-}
